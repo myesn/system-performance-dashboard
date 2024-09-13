3822fd72238413de3e31123b8cc61aad2edbe6fa
myesn <myesn@foxmail.com>
2024-09-13 16:04:41 +0800

    feat: 新增 /api/memory/mem 接口

---
+++ app/api/memory/mem/route.ts
@@ -0,0 +1,13 @@
+import si, { Systeminformation } from 'systeminformation'
+
+// Memory information
+export async function GET() {
+  // https://github.com/sebhildebrandt/systeminformation/blob/3a92931c7d46605ffddc1aacb97a9727273b2888/lib/memory.js#L146
+  // currentLoad: 当前负载百分比值
+  const { total, free, used } = await si.mem()
+  const result: MemoryMemResponse = { total, free, used }
+
+  return Response.json(result)
+}
+
+export interface MemoryMemResponse extends Pick<Systeminformation.MemData, 'total' | 'free' | 'used'> {}
