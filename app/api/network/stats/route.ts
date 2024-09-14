1740e562baca8f3476a24ccf341504b09bbe8704
myesn <myesn@foxmail.com>
2024-09-14 09:58:22 +0800

    feat: 增加 /api/network/stats 接口

---
+++ app/api/network/stats/route.ts
@@ -0,0 +1,31 @@
+import si, { Systeminformation } from 'systeminformation'
+
+export const dynamic = 'force-dynamic'
+
+// Network Stats
+export async function GET() {
+  const networkInterfaceDefault = await si.networkInterfaceDefault()
+  const [{ rx_bytes, tx_bytes, rx_sec, tx_sec }] = await si.networkStats(networkInterfaceDefault)
+  const result: NetworkStatsResponse = {
+    networkInterfaceDefault,
+    networkStats: {
+      rx_bytes,
+      tx_bytes,
+      rx_sec,
+      tx_sec,
+    },
+  }
+
+  return Response.json(result)
+}
+
+export interface NetworkStatsResponse {
+  // 默认网卡的名称
+  networkInterfaceDefault: string
+
+  // rx_bytes：接收的总字节数
+  // tx_bytes：发送的总字节数
+  // rx_sec: 接收字节数/秒（下载速度）
+  // tx_bytes: 每秒传输的字节数（上传速度）
+  networkStats: Pick<Systeminformation.NetworkStatsData, 'rx_bytes' | 'tx_bytes' | 'rx_sec' | 'tx_sec'>
+}
