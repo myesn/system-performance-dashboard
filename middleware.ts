09bf4fcb64e4257b1bb3c038b846baab8af72692
myesn <myesn@foxmail.com>
2024-09-14 18:13:56 +0800

    fix: 本地不限制速率请求

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,7 @@ const rateLimitMap = new Map()
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   const ip = request.headers.get('x-forwarded-for') || request.ip
-  // if (ip === '::1') return NextResponse.next()
+  if (ip === '::1') return NextResponse.next()
 
   // 目前 / 页面打开后，总共会发出 5 个请求，限制 15 代表 1 分钟内只允许刷新 3 次
   const limit = 15 // Limiting requests to 15 per minute per IP
