3380e81ede396ebe21e6f2688f9721617777ebe3
myesn <myesn@foxmail.com>
2024-09-15 10:46:10 +0800

    refactor: 添加网络 icon

--- components/system-information/network/stats.tsx
+++ components/system-information/network/stats.tsx
@@ -1,6 +1,8 @@
 'use client'
 
+import type { JSX } from 'react'
 import prettyBytes from 'pretty-bytes'
+import { Network, MoveUp, MoveDown, ArrowUp01, ArrowDown01 } from 'lucide-react'
 import { cn } from '@/lib/utils'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { useNetworkStats } from './use-network-stats'
@@ -17,28 +19,36 @@ export function NetworkStatsContainer() {
 }
 
 function NetworkStats({ networkInterfaceDefault, networkStats }: NetworkStatsProps) {
+  const itemIconClassName = 'h-4 w-4 text-muted-foreground'
+
   return (
     <Card>
       <CardHeader>
-        <CardTitle>网络流量</CardTitle>
+        <CardTitle className="flex items-center space-x-1">
+          <span>网络流量</span>
+          <Network className="h-4 w-4 text-muted-foreground" />
+        </CardTitle>
       </CardHeader>
       <CardContent className="grid grid-cols-2 lg:grid-cols-4 gap-2">
         {/* className="col-span-2" */}
         <Item name="网卡名称" value={networkInterfaceDefault} className="col-span-2 lg:col-span-4" />
-        <Item name="上传" value={prettyBytes(networkStats.tx_sec) + '/s'} />
-        <Item name="下载" value={prettyBytes(networkStats.rx_sec) + '/s'} />
-        <Item name="上传总量" value={prettyBytes(networkStats.tx_bytes)} />
-        <Item name="下载总量" value={prettyBytes(networkStats.rx_bytes)} />
+        <Item name="上传" icon={<MoveUp className={itemIconClassName} />} value={prettyBytes(networkStats.tx_sec) + '/s'} />
+        <Item name="下载" icon={<MoveDown className={itemIconClassName} />} value={prettyBytes(networkStats.rx_sec) + '/s'} />
+        <Item name="上传总量" icon={<ArrowUp01 className={itemIconClassName} />} value={prettyBytes(networkStats.tx_bytes)} />
+        <Item name="下载总量" icon={<ArrowDown01 className={itemIconClassName} />} value={prettyBytes(networkStats.rx_bytes)} />
       </CardContent>
     </Card>
   )
 }
 
-function Item({ name, value, className }: ItemProps) {
+function Item({ name, value, className, icon }: ItemProps) {
   return (
     <div className={cn(className)}>
       <div className="text-lg font-semibold">{value}</div>
-      <div className="text-sm text-muted-foreground">{name}</div>
+      <div className="text-sm text-muted-foreground flex items-center space-x-1">
+        {icon}
+        {name}
+      </div>
     </div>
   )
 }
@@ -47,6 +57,7 @@ interface ItemProps {
   name: string
   value: string | number
   className?: string
+  icon?: JSX.Element
 }
 
 interface NetworkStatsProps extends NetworkStatsResponse {}
