74e60a271d5ba3920d634d08e83c6ad043f81a65
myesn <myesn@foxmail.com>
2024-09-16 10:41:44 +0800

    refactor: 使用 tremor/category-bar 改造磁盘使用情况

--- components/system-information/file-system/size.tsx
+++ components/system-information/file-system/size.tsx
@@ -4,7 +4,7 @@ import prettyBytes from 'pretty-bytes'
 import { HardDrive } from 'lucide-react'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { useFileSystemSize } from './use-file-system-size'
-import { Progress } from '@/components/ui/progress'
+import { CategoryBar } from '@/components/tremor/category-bar'
 import type { FileSystemSizeResponse } from '@/app/api/file-system/size/route'
 
 export function FileSystemSizeContainer() {
@@ -37,6 +37,7 @@ function FileSystemSize({ fsSizes }: FileSystemSizeProps) {
 
 function Item({ fs, type, size, used, available, use, rw }: ItemProps) {
   const useRound = Math.round(use)
+
   return (
     <div>
       <div className="flex justify-between mb-1 text-sm">
@@ -44,11 +45,18 @@ function Item({ fs, type, size, used, available, use, rw }: ItemProps) {
           {fs} ({type}) {!rw && '只读'}
         </span>
         <span>剩余：{prettyBytes(available)}</span>
-        <span>
+        {/* <span>
           已用：{useRound}% ({prettyBytes(used)} / {prettyBytes(size)})
-        </span>
+        </span> */}
       </div>
-      <Progress value={useRound} className="h-2" indicatorClassName="bg-blue-500" />
+      {/* <Progress value={useRound} className="h-2" indicatorClassName="bg-blue-500" /> */}
+      {/* 背景颜色的 css 代码没有被编译进最终的 css 文件中。https://github.com/tremorlabs/tremor-raw/issues/89 */}
+      <CategoryBar
+        values={[25, 25, 25, 25]}
+        marker={{ value: useRound, tooltip: `已用 ${useRound}% (${prettyBytes(used)} / ${prettyBytes(size)})`, showAnimation: true }}
+        colors={['cyan', 'lime', 'amber', 'pink']}
+        showLabels={true}
+      />
     </div>
   )
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,9 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+/** for tremor ui components */
+export const cx = cn
